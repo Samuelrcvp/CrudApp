b690a00a28d284e648eee25ac8f2af7b0d7ef517
Samuelrcvp <samuelrcvp@gmail.com>
2023-10-17 14:29:36 -0300

    Criptografia hash+salt na senha login

--- Repositorio/IUsuarioRepositorio.cs
+++ Repositorio/IUsuarioRepositorio.cs
@@ -7,8 +7,8 @@ namespace CrudApp.Repositorio
         UsuarioModel BuscarPorLogin(string login);
         List<UsuarioModel> BuscarTodos();
         UsuarioModel ListarPorId(int id);
-        UsuarioModel Adicionar(UsuarioModel contato);
-        UsuarioModel Atualizar(UsuarioModel contato);
+        UsuarioModel Adicionar(UsuarioModel usuario);
+        UsuarioModel Atualizar(UsuarioModel usuario);
         bool Apagar(int id);
     }
 }
